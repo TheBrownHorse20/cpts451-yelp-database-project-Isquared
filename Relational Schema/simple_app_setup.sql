bdb7ce5bfad1514cb162d54a019fd004df9512f7
TheBrownHorse20 <159395195+TheBrownHorse20@users.noreply.github.com>
2026-03-14 02:56:59 -0700

    simple_app_setup.sql file commit
    I will push the full .sql version later today, but this version matches your code version so far :) :) PLEASE just read lines 12-13 and replace the .csv path with the actual filepath in your machine.

---
+++ Relational Schema/simple_app_setup.sql
@@ -0,0 +1,16 @@
+/* Ian: Run these commands in PostgreSQL using psql. 
+        This sets up the simple version of the milestone1db database and business table that matches
+        your WPF app setup.
+
+        I will be pushing the full .sql version later today. 
+*/
+
+CREATE DATABASE milestone1db;
+
+\c milestone1db
+
+CREATE TABLE business(name TEXT, city TEXT, state TEXT);
+
+--Run the following command in psql to import the CSV file
+--Just replace the <path>/milestone1DB.csv portion to include the actual path in you machine :)
+\copy business (name, state, city) FROM '<path>/milestone1DB.csv' DELIMITER ',' CSV;
